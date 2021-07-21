dfe803d07707cd82f7eca61c9bb8531a1f6c6535
Piet Brauer <piet@nerdishbynature.com>
2021-07-21 17:31:09 +0200

    Initial Commit

---
+++ GradeCalculator/GradeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradeCalculatorApp.swift
+//  GradeCalculator
+//
+//  Created by Piet Brauer-Kallenberg on 21.07.21.
+//
+
+import SwiftUI
+
+@main
+struct GradeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
